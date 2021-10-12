19f1f25b9f92ebb0f83d8a530f4ef0d3c13057b4
marc16w <marcmenendez2005@gmail.com>
2021-10-13 01:59:48 +0200

    first commit

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -4,15 +4,15 @@ module.exports = (client, message)  => {
  
    
             const embedDatos = new Discord.MessageEmbed() 
-            .setTitle("Bienvenid@ **XYZ HOST**")
+            .setTitle("Bienvenid@ **TRON HOSTING**")
             .setColor(0x033bfd)
             .setDescription(`${message.user} Pasalo bien y Disfruta!! `)
-            .setThumbnail("https://cdn.discordapp.com/attachments/746349531087503448/808432645243273236/photos_host.png") // Aqui va la img del imgur
-            .setFooter("XYZ HOST", client.user.avatarURL())
+            .setThumbnail("imagen del servidor") // Aqui va la img del imgur
+            .setFooter("TRON HOSTING", client.user.avatarURL())
             .setTimestamp()    
     
             // message.channel.send({ embed: embedDatos }); 
-            client.channels.cache.get('808440521508782081').send({ embed: embedDatos })  // lo envia al canal que le pasamos por cache
+            client.channels.cache.get('808440521508782081').send({ embed: embedDatos })  // id del canal de bienvenida
 
     }
 
